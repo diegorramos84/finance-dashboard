a9fa3413fecd895d143f47a8c6f87230f8322a08
Diego Ramos <diegorramos84@gmail.com>
2023-08-09 17:08:24 +0100

    change data model to allow historical data tracking

--- dashboard_backend/app/models.py
+++ dashboard_backend/app/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
+from datetime import date, timedelta
 
-# investment Types (ISA, 401k)
 class InvestmentType(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField()
@@ -9,16 +9,13 @@ class InvestmentType(models.Model):
     def __str__(self):
         return self.name
 
-
-# institutions (brokers, banks that are holding the investment)
 class Institution(models.Model):
-    name = models.CharField(max_length=100)
+    name = models.CharField(max_length=200)
     description = models.TextField()
 
     def __str__(self):
         return self.name
 
-# Portfolio
 class Portfolio(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     name = models.CharField(max_length=200)
@@ -28,19 +25,23 @@ class Portfolio(models.Model):
     def __str__(self):
         return self.name
 
-# Instrument (stocks, bonds)
 class Instrument(models.Model):
     portfolio = models.ForeignKey(Portfolio, on_delete=models.CASCADE)
-    investment_type = models.ForeignKey(InvestmentType, on_delete=models.CASCADE)
-    Institution = models.ForeignKey(Institution, on_delete=models.CASCADE)
+    investment_type = models.ForeignKey(InvestmentType, on_delete=models.PROTECT)
+    institution = models.ForeignKey(Institution, on_delete=models.PROTECT)
     ticker_symbol = models.CharField(max_length=20)
     name = models.CharField(max_length=200)
     position = models.DecimalField(max_digits=12, decimal_places=2)
-    market_value = models.DecimalField(max_digits=12, decimal_places=2)
-    # api data
-    last_known_market_value = models.DecimalField(max_digits=12, decimal_places=2)
-    last_updated_at = models.DateTimeField(null=True, blank=True)
-
 
     def __str__(self):
-        return f"{self.name} - {self.ticker_symbol}"
+        return f"{self.name} ({self.ticker_symbol})"
+
+class PortfolioDailyValue(models.Model):
+    portfolio = models.ForeignKey(Portfolio, on_delete=models.CASCADE)
+    date = models.DateField()
+    value = models.DecimalField(max_digits=12, decimal_places=2)
+
+class InstrumentDailyValue(models.Model):
+    instrument = models.ForeignKey(Instrument, on_delete=models.CASCADE)
+    date = models.DateField()
+    value = models.DecimalField(max_digits=12, decimal_places=2)
