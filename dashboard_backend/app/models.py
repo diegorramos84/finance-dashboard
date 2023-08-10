b43a6ef99c2cb8b6e64a26ad3513fb6f7c5c9760
Diego Ramos <diegorramos84@gmail.com>
2023-08-10 16:05:15 +0100

    add currency table

--- dashboard_backend/app/models.py
+++ dashboard_backend/app/models.py
@@ -2,6 +2,15 @@ from django.db import models
 from django.contrib.auth.models import User
 from datetime import date, timedelta
 
+
+class Currency(models.Model):
+    code = models.CharField(max_length=3, unique=True)
+    name = models.CharField(max_length=100)
+    symbol = models.CharField(max_length=10)
+
+    def __str__(self):
+        return self.code
+
 class InvestmentType(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField()
@@ -32,6 +41,7 @@ class Instrument(models.Model):
     ticker_symbol = models.CharField(max_length=20)
     name = models.CharField(max_length=200)
     position = models.DecimalField(max_digits=12, decimal_places=2)
+    currency = models.ForeignKey(Currency, on_delete=models.PROTECT)
 
     def __str__(self):
         return f"{self.name} ({self.ticker_symbol})"
